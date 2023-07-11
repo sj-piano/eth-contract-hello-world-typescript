2f720deeeadca253739feb38e799249ddfe1358a
StJohn Piano <stjohnpiano@gmail.com>
2023-07-11 13:31:24 +0200

    Build out config to support typescript (with absolute paths)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
+
 dotenv.config({path: './user-config.env'});
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;

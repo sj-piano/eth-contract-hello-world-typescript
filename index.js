c130bc501dc79cfa4cfb34c335dab732e780e915
StJohn Piano <stjohnpiano@gmail.com>
2023-07-11 14:15:13 +0200

    index.js -> index.ts

--- index.js
+++
@@ -1,7 +0,0 @@
-// Local imports
-const ethereum = require("#root/src/ethereum.js");
-
-// Exports
-module.exports = {
-  ...ethereum,
-};
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Local imports
+import * as ethereum from "#root/src/ethereum";
+
+// Exports
+export default {
+  ...ethereum,
+};
